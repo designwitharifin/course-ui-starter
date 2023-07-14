2af0b9b8c1dcedf1194f4a831f14e4d6b13ea5f5
designwitharifin <designwitharifin@gmail.com>
2023-07-14 08:01:50 +0700

    js

--- assets/js/script.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $(window).on("scroll", function () {
-        if ($(window).scrollTop() >= 20) {
-            $(".navbar").addClass("nav-shrink");
-        } else {
-            $(".navbar").removeClass("nav-shrink");
-        }
-    });
-});
